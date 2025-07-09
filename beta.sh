68f89a985ec300ebd50c14ea706da2b218295680
ErfanXRay <1983383+Erfan-XRay@users.noreply.github.com>
2025-07-09 06:54:22 +0330

    Update beta.sh

--- beta.sh
+++ beta.sh
@@ -1608,7 +1608,7 @@ while true; do
                 echo -e "${YELLOW}Press Enter to continue...${RESET}" # Press Enter to continue...
                 read -p ""
               ;;
-            esac
+            </case>
           done
           ;;
         3)
